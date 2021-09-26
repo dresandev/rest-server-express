8bd48303a675bcc6d6fdaee1b1f72b3838ea3dc7
Javier Andres <javiandres.016@gmail.com>
2021-09-26 00:49:01 -0500

    prueba

--- models/server.js
+++ models/server.js
@@ -15,6 +15,7 @@ class Server {
         //app routes
         this.routes();
         this.port = process.env.PORT;
+        this.host = process.env.HOST;
     }
 
     async connectDB() {
@@ -35,7 +36,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen(this.port);
+        this.app.listen(this.port, this.host);
     }
 }
 
